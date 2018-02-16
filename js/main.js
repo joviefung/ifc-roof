073aa72e799c33814dfa19c6d49d7476baa31f69
joviefung <joviefung@gmail.com>
2018-02-16 19:24:20 +0800

    add text aliasing using canvas

--- js/main.js
+++ js/main.js
@@ -1,7 +1,53 @@
 import html2canvas from 'html2canvas';
 
 const convertImage = () => {
-  html2canvas($(".image-container").get(0)).then((canvas) => $(".download-link").attr("href", canvas.toDataURL()));
+  // get the background image first
+  $(".text-row").hide();
+  html2canvas($(".image-container").get(0)).then((canvas) => {
+    // hide the background image, change to blue background to make it easier for transparency post-processing
+    $(".image-container").removeClass("show");
+    // restore text
+    $(".text-row").show();
+
+    html2canvas($(".image-container").get(0)).then((textCanvas) => {
+      // restore background image
+      $(".image-container").addClass("show");
+
+      // resize the text to smaller size to alias the font
+      const smallCanvas = document.createElement('canvas');
+      const context = smallCanvas.getContext("2d");
+      smallCanvas.width = textCanvas.width / 2;
+      smallCanvas.height = textCanvas.height / 2;
+      context.drawImage(textCanvas, 0, 0, textCanvas.width, textCanvas.height, 0, 0, textCanvas.width / 2, textCanvas.height / 2);
+
+      // remove blue background
+      var image = context.getImageData(0, 0, smallCanvas.width, smallCanvas.height);
+      length = image.data.length;
+      for(var i = 3; i < length; i += 4){
+        if (image.data[i-1] > image.data[i-2] && image.data[i-1] > image.data[i-3]) {
+          if (image.data[i-1] > 210) {
+            // make background transparent
+            image.data[i] = 0; 
+          } else {
+            // make to grey or black to increase the black border of text
+            image.data[i-1] = image.data[i-3];
+            image.data[i-2] = image.data[i-3];
+          }
+        }
+      }
+      context.putImageData(image, 0, 0);
+      
+      // merge the background image and text
+      const finalCanvas = document.createElement('canvas');
+      const finalContext = finalCanvas.getContext("2d");
+      finalCanvas.width = canvas.width;
+      finalCanvas.height = canvas.height;
+      finalContext.drawImage(canvas, 0, 0);
+      finalContext.drawImage(smallCanvas, 0, 0, smallCanvas.width, smallCanvas.height, 0, 0, canvas.width, canvas.height);
+
+      $(".download-link").attr("href", finalCanvas.toDataURL());
+    });
+  });
 };
 
 $(function() {
